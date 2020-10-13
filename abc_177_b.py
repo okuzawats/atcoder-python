0c5c9292cdccffad27b33d66ec453c88a207c241
okuzawats <okuzawats@gmail.com>
2020-10-13 21:09:12 +0900

    abc 177 b

--- abc_177_b.py
+++ abc_177_b.py
@@ -1,10 +1,11 @@
 S = input()
 T = input()
-m = len(T)
-for i in range(len(S)-len(T)):
+t = len(T)
+a = t
+for i in range(len(S) - t + 1):
     l = len(T)
     for j in range(l):
         if S[i+j] == T[j]:
             l -= 1
-    m = min(m, l)
-print(m)
+    a = min(a, l)
+print(a)
