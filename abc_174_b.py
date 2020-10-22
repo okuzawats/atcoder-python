0cb867140fee15d7f4f87c7583dbb1813f3569a9
okuzawats <okuzawats@gmail.com>
2020-10-23 06:45:03 +0900

    update abc 174 b

--- abc_174_b.py
+++ abc_174_b.py
@@ -1,7 +1,6 @@
 N, D = map(int, input().split())
 c = 0
-for i in range(N):
+for _ in range(N):
     X, Y = map(int, input().split())
-    if X**2 + Y**2 <= D**2:
-        c += 1
+    c += X*X + Y*Y <= D*D
 print(c)
