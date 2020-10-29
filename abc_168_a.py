1e6a0dc8b11bf143ddc2e415d866017093685b71
okuzawats <okuzawats@gmail.com>
2020-10-30 06:34:45 +0900

    [AC] [update] ABC 168 A

--- abc_168_a.py
+++ abc_168_a.py
@@ -1,7 +1,1 @@
-n = int(input()) % 10
-if n in (2, 4, 5, 7, 9):
-    print('hon')
-elif n in (0, 1, 6, 8):
-    print('pon')
-else:
-    print('bon')
+print('{}on'.format('pphbhhphph'[int(input()) % 10]))
