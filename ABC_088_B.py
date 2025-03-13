c7347a9e59c3becf7df3a3a6710eeeff83c3bf51
okuzawats <okuzawats@gmail.com>
2025-03-13 18:52:36 +0900

    ABC 088 B

--- ABC_088_B.py
+++ ABC_088_B.py
@@ -3,8 +3,5 @@ a = [*map(int, input().split())]
 a.sort(reverse=True)
 s = 0
 for i in range(n):
-    if i % 2 < 1:
-        s += a[i]
-    else:
-        s -= a[i]
+    s += a[i] if i % 2 < 1 else -a[i]
 print(s)
