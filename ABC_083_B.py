1da967a2e04c2ceb3789289f5627eb66c3263062
okuzawats <okuzawats@gmail.com>
2025-03-13 18:16:51 +0900

    [AC] ABC 083 B

--- ABC_083_B.py
+++ ABC_083_B.py
@@ -1,8 +1,8 @@
-n, a, b = [*map(int, input().split())]
+n, a, b = map(int, input().split())
 
 ans = 0
 for i in range(1, n + 1):
     s = sum(map(int, [*str(i)]))
-    if a <= s and s <= b:
+    if a <= s <= b:
         ans += i
 print(ans)
