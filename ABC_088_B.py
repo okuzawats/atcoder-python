32ed530ddc12e47d41643bf7bd88298fce32455c
okuzawats <okuzawats@gmail.com>
2025-03-13 18:55:08 +0900

    [AC] ABC 088 B

--- ABC_088_B.py
+++ ABC_088_B.py
@@ -1,7 +1,4 @@
 n = int(input())
 a = [*map(int, input().split())]
 a.sort(reverse=True)
-s = 0
-for i in range(n):
-    s += a[i] if i % 2 < 1 else -a[i]
-print(s)
+print(sum([a[i] if i % 2 < 1 else -a[i] for i in range(n)]))
