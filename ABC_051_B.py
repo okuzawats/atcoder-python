1171622f8b75f000e560ade6e169ad1d07212530
okuzawats <okuzawats@gmail.com>
2025-03-19 16:54:57 +0900

    [AC] ABC 051 B

--- ABC_051_B.py
+++ ABC_051_B.py
@@ -3,8 +3,7 @@ k, s = map(int, input().split())
 c = 0
 for x in range(k + 1):
     for y in range(k + 1):
-        for z in range(k + 1):
-            if x + y + z == s:
-                c += 1
+        if 0 <= s - x - y <= k:
+            c += 1
 
 print(c)
