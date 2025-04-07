6bb8a2847f835fa07c77e44a8a28018560611830
okuzawats <okuzawats@gmail.com>
2025-04-07 14:37:51 +0900

    [AC] ABC 124 B

--- ABC_124_B.py
+++ ABC_124_B.py
@@ -1,8 +1,8 @@
 n = int(input())
 h = [*map(int, input().split())]
-c, h_max = 0, 0
-for i in range(n):
-    if h[i] >= h_max:
+c, m = 0, 0
+for i in h:
+    if i >= m:
         c += 1
-    h_max = max(h_max, h[i])
+    m = max(m, i)
 print(c)
