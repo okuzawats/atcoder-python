36ff9b2ec3932319107802f0c94cc3d43a2a1193
okuzawats <okuzawats@gmail.com>
2025-04-11 22:04:40 +0900

    [AC] ABC 115 C

--- ABC_115_C.py
+++ ABC_115_C.py
@@ -1,10 +1,7 @@
 N, K = map(int, input().split())
 H = sorted([int(input()) for _ in range(N)])
 
-ans = 10**9
+a = 10**9
 for i in range(N - K + 1):
-    mx = H[i + K - 1]
-    mn = H[i]
-    ans = min(ans, mx - mn)
-
-print(ans)
+    a = min(a, H[i + K - 1] - H[i])
+print(a)
