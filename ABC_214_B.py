bb5d55d2519fedb8635d3b16852fbf7a53c6691a
okuzawats <okuzawats@gmail.com>
2025-04-20 06:24:47 +0900

    [WA] ABC 214 B

---
+++ ABC_214_B.py
@@ -0,0 +1,8 @@
+S, T = map(int, input().split())
+ans = 0
+for a in range(100):
+    for b in range(100):
+        for c in range(100):
+            if a + b + c <= S and  a * b * c <= T:
+                ans += 1
+print(ans)
