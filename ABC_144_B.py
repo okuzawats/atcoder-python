d4b8ad4e0aba8fdad43e11b9021597d3be92d57c
okuzawats <okuzawats@gmail.com>
2025-04-20 05:38:40 +0900

    [AC] ABC 144 B

--- ABC_144_B.py
+++ ABC_144_B.py
@@ -1,7 +1,2 @@
 N = int(input())
-for i in range(1, 10):
-    for j in range(1, 10):
-        if N == i * j:
-            print("Yes")
-            exit()
-print("No")
+print("Yes" if N in set([i * j for i in range(1, 10) for j in range(1, 10)]) else "No")
