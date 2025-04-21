67a5351cd2a536f11244a9ffedf4c88296095628
okuzawats <okuzawats@gmail.com>
2025-04-21 21:51:35 +0900

    [AC] ABC 394 B

--- ABC_394_B.py
+++ ABC_394_B.py
@@ -1,11 +1,4 @@
-N, S = int(input()), []
-for _ in range(N):
-    s = input()
-    S.append([len(s), s])
-S.sort()
-
-ans = ""
-for s in S:
-    ans += s[1]
-
-print(ans)
+N = int(input())
+S = [input() for _ in range(N)]
+S.sort(key=len)
+print("".join(S))
