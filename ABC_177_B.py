948d4da1a570bf20789924dc4c2aa5be2e89d4b9
okuzawats <okuzawats@gmail.com>
2025-04-22 09:22:33 +0900

    [AC] ABC 177 B

--- ABC_177_B.py
+++ ABC_177_B.py
@@ -1,7 +1,7 @@
 S, T = input(), input()
 
 a = len(T)
-for i in range(len(S) - len(T) - 1):
+for i in range(len(S) - len(T) + 1):
     c = 0
     for j in range(len(T)):
         if T[j] != S[i + j]:
