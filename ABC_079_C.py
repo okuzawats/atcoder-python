559d121e1f1e35a0fe64e2cbf0a5cd9b2dbe75a9
okuzawats <okuzawats@gmail.com>
2025-04-29 06:20:36 +0900

    [AC] ABC 079 C

--- ABC_079_C.py
+++ ABC_079_C.py
@@ -3,25 +3,17 @@ A, B, C, D = int(S[0]), int(S[1]), int(S[2]), int(S[3])
 
 if A + B + C + D == 7:
     print(f"{A}+{B}+{C}+{D}=7")
-    exit()
-if A + B + C - D == 7:
+elif A + B + C - D == 7:
     print(f"{A}+{B}+{C}-{D}=7")
-    exit()
-if A + B - C + D == 7:
+elif A + B - C + D == 7:
     print(f"{A}+{B}-{C}+{D}=7")
-    exit()
-if A + B - C - D == 7:
+elif A + B - C - D == 7:
     print(f"{A}+{B}-{C}-{D}=7")
-    exit()
-if A - B + C + D == 7:
+elif A - B + C + D == 7:
     print(f"{A}-{B}+{C}+{D}=7")
-    exit()
-if A - B + C - D == 7:
+elif A - B + C - D == 7:
     print(f"{A}-{B}+{C}-{D}=7")
-    exit()
-if A - B - C + D == 7:
+elif A - B - C + D == 7:
     print(f"{A}-{B}-{C}+{D}=7")
-    exit()
-if A - B - C - D == 7:
+elif A - B - C - D == 7:
     print(f"{A}-{B}-{C}-{D}=7")
-    exit()
