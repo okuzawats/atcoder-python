b0843e0ca3060fea048cb5a0a79ce6cd893ec1e8
okuzawats <okuzawats@gmail.com>
2025-04-29 06:19:26 +0900

    [AC] ABC 079 C

---
+++ ABC_079_C.py
@@ -0,0 +1,27 @@
+S = input()
+A, B, C, D = int(S[0]), int(S[1]), int(S[2]), int(S[3])
+
+if A + B + C + D == 7:
+    print(f"{A}+{B}+{C}+{D}=7")
+    exit()
+if A + B + C - D == 7:
+    print(f"{A}+{B}+{C}-{D}=7")
+    exit()
+if A + B - C + D == 7:
+    print(f"{A}+{B}-{C}+{D}=7")
+    exit()
+if A + B - C - D == 7:
+    print(f"{A}+{B}-{C}-{D}=7")
+    exit()
+if A - B + C + D == 7:
+    print(f"{A}-{B}+{C}+{D}=7")
+    exit()
+if A - B + C - D == 7:
+    print(f"{A}-{B}+{C}-{D}=7")
+    exit()
+if A - B - C + D == 7:
+    print(f"{A}-{B}-{C}+{D}=7")
+    exit()
+if A - B - C - D == 7:
+    print(f"{A}-{B}-{C}-{D}=7")
+    exit()
