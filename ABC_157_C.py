185acd71498751be9d7c9e7ea1f37db6c590caa7
okuzawats <okuzawats@gmail.com>
2025-05-01 06:15:56 +0900

    [AC] ABC 157 C

--- ABC_157_C.py
+++ ABC_157_C.py
@@ -3,9 +3,7 @@ S = [[*map(int, input().split())] for _ in range(M)]
 
 for i in range(10**N):
     n = str(i)
-    if len(n) != N:
-        continue
-    if any(n[s - 1] != str(c) for s, c in S):
+    if len(n) != N or any(n[s - 1] != str(c) for s, c in S):
         continue
     print(n)
     exit()
