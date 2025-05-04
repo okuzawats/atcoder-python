e0586743ff3f31d8f990849bf8c255561a9e846d
okuzawats <okuzawats@gmail.com>
2025-05-04 15:32:30 +0900

    [AC] ABC 095 C

--- ABC_095_C.py
+++ ABC_095_C.py
@@ -1,5 +1,13 @@
+from functools import reduce
+
 A, B, C, X, Y = map(int, input().split())
-ans = 10**18
-for c in range(max(X, Y) * 2 + 1):
-    ans = min(ans, max(0, X - c // 2) * A + max(0, Y - c // 2) * B + c * C)
-print(ans)
+
+print(
+    reduce(
+        lambda acc, c: min(
+            acc, max(0, X - c // 2) * A + max(0, Y - c // 2) * B + c * C
+        ),
+        range(max(X, Y) * 2 + 1),
+        10**18,
+    )
+)
