aa78635e85c81a18804605d0521f3a562d733241
okuzawats <okuzawats@gmail.com>
2025-05-04 15:19:10 +0900

    [WA] ABC 095 C

---
+++ ABC_095_C.py
@@ -0,0 +1,8 @@
+A, B, C, X, Y = map(int, input().split())
+
+ans = 5000 * 10**5
+for c in range(max(X, Y) * 2 + 1):
+    a = max(0, X - c // 2)
+    b = max(0, Y - c // 2)
+    ans = min(ans, a * A + b * B + c * C)
+print(ans)
