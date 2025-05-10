880aead95a3ff8be8723dc8d74c897f6517c6de3
okuzawats <okuzawats@gmail.com>
2025-05-10 22:15:27 +0900

    [AC] ABC 073 B

--- ABC_073_B.py
+++ ABC_073_B.py
@@ -1,5 +1,5 @@
-N, cnt = int(input()), 0
-for _ in range(N):
+c = 0
+for _ in range(int(input())):
     l, r = map(int, input().split())
-    cnt += r - l + 1
-print(cnt)
+    c += r - l + 1
+print(c)
