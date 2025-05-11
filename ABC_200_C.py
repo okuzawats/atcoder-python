988aeddc0a9454a75613308d3b9221a6e05cbe88
okuzawats <okuzawats@gmail.com>
2025-05-11 09:27:03 +0900

    [RE] ABC 200 C

---
+++ ABC_200_C.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+N = int(input())
+A = [*map(int), input().split()]
+dct, cnt = defaultdict(int), 0
+
+for a in A:
+    mod = a % 200
+    cnt += dct[mod]
+    dct[mod] += 1
+print(cnt)
