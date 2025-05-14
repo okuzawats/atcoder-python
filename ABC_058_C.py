ae4fcc940cefcd0ef8734f8cf41a1547af8d1c2b
okuzawats <okuzawats@gmail.com>
2025-05-14 21:58:32 +0900

    [AC] ABC 058 C

--- ABC_058_C.py
+++ ABC_058_C.py
@@ -1,15 +1,8 @@
-N = int(input())
-S = [input() for _ in range(N)]
-count_min = [10**18] * 26
-
-for s in S:
-    count_s = [0] * 26
-    for c in s:
-        count_s[ord(c) - 97] += 1
+N, c_m = int(input()), [10**5] * 26
+for _ in range(N):
+    S, c_s = input(), [0] * 26
+    for c in S:
+        c_s[ord(c) - 97] += 1
     for i in range(26):
-        count_min[i] = min(count_min[i], count_s[i])
-
-ans = ""
-for i in range(26):
-    ans += chr(i + 97) * count_min[i]
-print(ans)
+        c_m[i] = min(c_m[i], c_s[i])
+print("".join([chr(i + 97) * c_m[i] for i in range(26)]))
