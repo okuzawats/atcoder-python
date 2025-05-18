5a2665085a7abeb54ce18d93ee9b73fb5ca0621c
okuzawats <okuzawats@gmail.com>
2025-05-19 06:21:07 +0900

    [WA] AGC 027 A

---
+++ AGC_027_A.py
@@ -0,0 +1,13 @@
+N, X = map(int, input().split())
+A = sorted([*map(int, input().split())])
+
+cnt = 0
+for a in A:
+    if X - a >= 0:
+        cnt += 1
+        X -= a
+    else:
+        break
+
+ans = cnt - 1 if X > 0 else cnt
+print(ans)
